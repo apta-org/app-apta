fc70dd7e7aac50c31e4d9da586afc700476e36f5
rkalli <ravi_k_kalli@homedepot.com>
2018-01-30 16:27:59 -0500

    [#154779933] Created seed data and api for state and districts

--- md-seed-config.js
+++ md-seed-config.js
@@ -3,6 +3,7 @@ const Promise = require('bluebird')
 const Config = require('./lib/config')
 const CourseSeeder = require('./lib/resources/seeders/course.seeders')
 const RuleSeeder = require('./lib/resources/seeders/rule.seeders.js')
+const StateSeeder = require('./lib/resources/seeders/state.seeders')
 
 MongooseLib.Promise = Promise
 
@@ -14,10 +15,13 @@ const mongoConfig = {
    * IMPORTANT: Order is very critical.
    *
    * @type {{Courses: CourseSeeder}}
+   * @type {{Rules: RuleSeeder}}
+   * @type {{States: StateSeeder}}
    */
   seedersList: {
     CourseSeeder,
-    RuleSeeder
+    RuleSeeder,
+    StateSeeder
   }
 }
 
