54184cd8aa505f94b7756a8f0ad489d4be00a30f
rkalli <ravi_k_kalli@homedepot.com>
2018-05-22 19:33:25 -0400

    Pipeline WIP

--- ci/scripts/check_mongodb.sh
+++ ci/scripts/check_mongodb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Checking if mongodb is running..."
+echo "Checking if mongodb is present..."
 if mongod --version > /dev/null
 then
     echo "MongoDB is installed!!!"
 else
     echo "MongoDB is not present... starting installation"
-        sh ./install_mongodb.sh
+        sh ci/scripts/install_mongodb.sh
 fi
